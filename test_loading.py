4965e27f0f2c613c3c082efe964406d93445f0bb
Mark McDowall <mark.mcdowall@gmail.com>
2016-11-22 14:16:55 +0000

    Scrit to test the random loading of data into a MongoDB

---
+++ test_loading.py
@@ -0,0 +1,16 @@
+import random
+
+from dmp import dmp
+
+users = ["adam", "ben", "chris", "denis", "eric"]
+file_types = ["fastq", "fasta", "bam", "bed", "hdf5", "tsv", "wig", "pdb"]
+data_types = ['RNA-seq', 'MNase-Seq', 'ChIP-seq', 'WGBS', 'HiC']
+
+da = dmp()
+
+for i in xrange(100000):
+    u = random.choice(users)
+    ft = random.choice(file_types)
+    dt = random.choice(data_types)
+    f = '/tmp/test/' + dt + '/test_' + str(i) + '.' + ft
+    da.set_file(u, f, ft, dt)
