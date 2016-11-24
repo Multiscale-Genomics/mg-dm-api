c34669b926533135ddd3921ba83dd3d9c5d0785b
Mark McDowall <mark.mcdowall@gmail.com>
2016-11-24 15:06:02 +0000

    Updated the test_load to reflect the changes to the API

--- test_loading.py
+++ test_loading.py
@@ -5,6 +5,7 @@ from dmp import dmp
 users = ["adam", "ben", "chris", "denis", "eric"]
 file_types = ["fastq", "fasta", "bam", "bed", "hdf5", "tsv", "wig", "pdb"]
 data_types = ['RNA-seq', 'MNase-Seq', 'ChIP-seq', 'WGBS', 'HiC']
+compressed = [None, 'gzip', 'zip']
 
 da = dmp()
 
@@ -12,9 +13,10 @@ for i in xrange(10):
     u = random.choice(users)
     ft = random.choice(file_types)
     dt = random.choice(data_types)
+    z  = random.choice(compressed)
     f = '/tmp/test/' + dt + '/test_' + str(i) + '.' + ft
-    file_id = da.set_file(u, f, ft, dt)
+    file_id = da.set_file(u, f, ft, dt, 9606, z)
     
     if dt == 'RNA-seq' and ft == 'fastq' and random.choice([0,1]) == 1:
          f = '/tmp/test/' + dt + '/test_' + str(i) + '.bam'
-         da.set_file(u, f, 'bam', dt, source)
+         da.set_file(u, f, 'bam', dt, 9606, None, [file_id])
