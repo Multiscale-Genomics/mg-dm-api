ffbacaa4cdd1b81379d67278100489326b6abc49
Mark McDowall <mark.mcdowall@gmail.com>
2016-11-29 15:37:58 +0000

    init test 2

--- dmp/__init__.py
+++ dmp/__init__.py
@@ -14,8 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from dmp import dmp
-from dmp import rest
+from dmp import *
 
 __author__  = 'Mark McDowall'
 __version__ = 'v0.0'
