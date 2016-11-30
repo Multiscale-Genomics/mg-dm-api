f8ab7f5f91b84816d6be82095f161ad97db6ac7c
Mark McDowall <mark.mcdowall@gmail.com>
2016-11-30 09:44:01 +0000

    RTD test to get rest to show - Changed the init imports

--- dmp/__init__.py
+++ dmp/__init__.py
@@ -14,8 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from dmp import dmp
-from dmp import rest
+#from dmp import dmp
+#from dmp import rest
 
 __author__  = 'Mark McDowall'
 __version__ = 'v0.0'
