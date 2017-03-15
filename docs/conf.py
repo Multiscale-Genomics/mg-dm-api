e9538395d79492d9eba260560a2e9466927d0010
Mark McDowall <mark.mcdowall@gmail.com>
2017-03-15 16:49:31 +0000

    Removed pyBiGWig from teh requirements then mocked it in the conf.py to allow RTD to build

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,16 @@ import os, sys
 sys.path.insert(0, os.path.abspath('..'))
 import dmp
 
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['pyBigWig']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
